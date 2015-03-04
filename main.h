628894ece7ca15b5aab48207195219a30ce3a059
ch98-1 <akerunakayama+1@gmail.com>
2015-03-04 14:56:06 -0800

    Added scoring variables

--- main.h
+++ main.h
@@ -17,7 +17,16 @@
 #define STAR 3 //star point in aray
 
 
-unsigned char board[BOARD_SIZE][BOARD_SIZE];//go board. blank is 0, black is 1, white is 2. board[y][x].
+unsigned char board[BOARD_SIZE][BOARD_SIZE];//go board. blank is 0, black is 1, white is 2. star points is 3. board[y][x].
+unsigned char simulation[BOARD_SIZE][BOARD_SIZE];//copy of go board for simulations and scoring.
+
+unsigned int black_captured;//number of black captured
+unsigned int white_captured;//number of white captured
+
+unsigned int black_area_score;//area / chinese scoring for black
+unsigned int white_area_score;//area / chinese scoring for white
+unsigned int black_territory_score;//territory / japanese scoring for black
+unsigned int white_territory_score;//territory / japanese scoring for white
 
 void printboard(void);//print out bord following the macros with space between each
 
