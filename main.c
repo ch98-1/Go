04d30cd0a952fb550cd4bf27fd9f437a0650da40
ch98-1 <akerunakayama+1@gmail.com>
2015-03-06 14:48:11 -0800

    added piece placing code

--- main.c
+++ main.c
@@ -1,24 +1,165 @@
 //Copyright (C) 2014-2015  Corwin Hansen
 #include "main.h"
 
+
+
 int main(void){//no arguments to start
-	printf("\n");//first character
+	init();//initialise variables and load other things
 
-	init();//initialise variables
+	char input[128];//message input
+	while (1){
+		printf(">");//prompt command
+		gets(input);//get input
+		writelog(input);//write input tolog
 
-	//place pieces for test
-	place(1,1,BLACK);//black
-	place(2,1,WHITE);//white
-	place(4, 4, BLANK);//blank
+		//process input
+		if (strcmp(input, "help") == 0 || strcmp(input, "-h") == 0 || strcmp(input, "h") == 0 || strcmp(input, "-help") == 0 || strcmp(input, "") == 0){//if it is one of thise help messages
+			printf(HELP);//print help
+			writelog(HELP);//log help message
+		}
 
-	printboard();//print out the board
 
-	territoryscore();//score for territory
-	
-	areascore();//score for area
+		else  if (strcmp(input, "quit") == 0){//if quitting
+			printf("Unsaved data will be eraced. Do you really want to quit? (y/n): ");//prompt if you want to quit or not
+			gets(input);//get input
+			writelog(input);//log input
+			if (strcmp(input, "y") == 0) break;//if input is y, break
+		}
 
+
+		else if (strcmp(input, "save") == 0){//if saving
+			printf("Please input the filename: ");//prompt to enter the file
+			gets(input);//get input
+			writelog(input);//log input
+			save(input);//save input
+		}
+
+
+		else if (strcmp(input, "load") == 0){//if loading
+			printf("Please input the filename: ");//prompt to enter the file
+			gets(input);//get input
+			writelog(input);//log input
+			load(input);//save input
+		}
+
+
+		else if (strcmp(input, "new") == 0){//if making new game
+			printf("Unsaved data will be eraced. Do you really want to make a new game? (y/n): ");//prompt if you want to makem new game or not
+			gets(input);//get input
+			writelog(input);//log input
+			if (strcmp(input, "y") == 0) init();//if input is y, initialise
+		}
+
+
+		else if (strcmp(input, "print") == 0){//if printing bord
+			printboard();//print bord
+		}
+
+
+		else if (strcmp(input, "pass") == 0){//if passing
+			pass(turn);
+			if (mode == 2){//if 2 player mode
+				if (turn == BLACK){//switch turn
+					turn = WHITE;
+				}
+				else {
+					turn = BLACK;
+				}
+			}
+			if (mode == 2){//if at mode 2
+				if (turn == BLACK){//display turn
+					printf("Black's turn\n");
+					writelog("Black's turn\n");//log turn
+				}
+				else {
+					printf("White's turn\n");
+					writelog("White's turn\n");//log turn
+				}
+			}
+		}
+
+
+		else if (strcmp(strtok(input, " "), "place") == 0){//if placing a piece
+			char *xstr = strtok(NULL, " ");//get x
+			char *ystr = strtok(NULL, " ");//get y
+			if (xstr != NULL && ystr != NULL){//if both are valid input
+				int x = atoi(xstr);//get x and y position
+				int y = atoi(ystr);
+				if (legal(x, y, turn)){//if it is legal move
+					place(x, y, turn);//place piece
+
+					if (turn == BLACK){//if black's turn
+						black_pass = 0;//black didn't pass
+					}
+					else{
+						white_pass = 0;//white didn't pass
+					}
+
+					if (mode == 2){//if 2 player mode
+						if (turn == BLACK){//switch turn
+							turn = WHITE;
+						}
+						else {
+							turn = BLACK;
+						}
+					}
+
+					printboard();//print the bord
+				}
+				else{//if it was a illegalmove
+					printf("Illegal move\n");//irregal move
+					writelog("Illegal move\n");//write to log
+				}
+			}
+			else{//if bad input
+				printf("Invalid input\n");//invalid input
+				writelog("Invalid input\n");//write to log
+			}
+		}
+
+
+
+		if (mode == 1){//if computer mode
+			writelog("Computer's turn\n");//log that computer is playing
+			play();//make computer play white
+		}
+
+
+		if (white_pass == 1 && black_pass == 1){//if end of game
+			printf("end of game. Calculsting score...\n");//tell that game ended
+			writelog("end of game. Calculsting score...\n");//write to log
+
+			territoryscore();//score for territory
+			areascore();//score for area
+
+			sprintf(input, "Area score:\n Black: %u   White: %u \n Territory score:\n Black: %u   White: %u\n", black_area_score, white_area_score, black_territory_score, white_territory_score);//make score string
+			printf("%s", input);//print score
+			writelog(input);//log score
+			
+			printf("do you want to save? (y/n): ");//prompt if you want to save or not
+			gets(input);//get input
+			writelog(input);//log input
+			if (strcmp(input, "y") == 0) {//if input is y
+				printf("Please input the filename: ");//prompt to enter the file
+				gets(input);//get input
+				writelog(input);//log input
+				save(input);//save input
+			}
+
+			init();//initialse
+		}
+
+
+	} 
+
+
+	fclose(logfile);//close the log file
 	exit(EXIT_SUCCESS);//end of program
 }
+
+
+
+
 
 void printboard(void){//print out bord following the macros with space between each
 	int i, j;
@@ -47,13 +188,28 @@ void printboard(void){//print out bord following the macros with space between e
 		}
 		printf("\n");//newline
 	}
-
+	if (mode == 2){//if at mode 2
+		if (turn == BLACK){//display turn
+			printf("Black's turn\n");
+			writelog("Black's turn\n");//log turn
+		}
+		else {
+			printf("White's turn\n");
+			writelog("White's turn\n");//log turn
+		}
+	}
 
 }
 
 
 
-void init(void){//initialise variables
+
+
+
+
+void init(void){//initialise variables and load other things
+	turn = BLACK;//black turn at start
+	printf("\n");//newline to make sure it is in next line
 	memset(board, 0, BOARD_SIZE*BOARD_SIZE);//clear board
 	//place / mark star points
 	//4,4 points
@@ -68,7 +224,33 @@ void init(void){//initialise variables
 	place(16, 10, STAR);
 	//10,10 points
 	place(10, 10, STAR);
+
+	//open log file
+	logfile = fopen(LOG, "ab");//open for appending file
+	if (logfile == NULL){//if it wasn't opened
+		printf("log file could not be opened or created. Check prtmission of %s\n", LOG);//send error message
+		exit(EXIT_FAILURE);//end of program
+	}
+	setbuf(logfile, NULL);//unbufferd file
+
+	printf("How many players? (1 for human vs computer, 2 for human vs human): ");
+	writelog("How many players? (1 for human vs computer, 2 for human vs human): ");//write prompt to log
+	char num[64];//number
+	gets(num);//get string for that number
+	writelog(num);//write input to log
+	mode = atoi(num);//get number from string
+	if (mode != 1 && mode != 2){//if not valid input
+		mode = 2;//default 2 players
+		printf("Invalid input. Using 2 player mode.\n");//give error
+		writelog("Invalid input. Using 2 player mode.\n");//log error
+	}
+	printf("Black's turn\n");//announce turn
+	writelog("Black's turn\n");//log turn
 }
+
+
+
+
 
 
 void place(unsigned char x, unsigned char y, unsigned char piece){//place a piece on board
@@ -108,9 +290,17 @@ void place(unsigned char x, unsigned char y, unsigned char piece){//place a piec
 }
 
 
+
+
+
+
 void territoryscore(void){//count score for territory / japanese scoring
 
 }
+
+
+
+
 
 
 void areascore(void){//count score for area / chinese scoring
@@ -119,11 +309,144 @@ void areascore(void){//count score for area / chinese scoring
 
 
 
-void save(const char file){//save board to file
 
+
+
+
+void save(const char* file){//save board to file
+	FILE *data = fopen(file, "wb");//open file for reading
+	if (data == NULL){
+		printf("could not open or create file. Check prtmission of %s\n", file);//give error message
+		writelog("could not open or create file. Check prtmission of file\n");//log error message
+		return;//end function
+	}
+	rewind(data);//set to start
+	int i, j;
+	for (i = 0; i < BOARD_SIZE; i++){//each row
+		for (j = 0; j < BOARD_SIZE; j++){//each position
+			int piece = fputc(board[i][j], data);//write piece
+			if (piece == EOF){
+				printf("data could not be written correctly. Check permission of %s\n", file);//give error message
+				writelog("data could not be written correctly. Check permission of the file\n");//log error message
+				return;//end function
+			}
+		}
+	}
+	int piece = fputc(turn, data);//write turn
+	if (piece == EOF){
+		printf("data could not be written correctly. Check permission of %s\n", file);//give error message
+		writelog("data could not be written correctly. Check permission of the file\n");//log error message
+		return;//end function
+	}
+	piece = fputc(mode, data);//write mode
+	if (mode == EOF){
+		printf("data could not be written correctly. Check permission of %s\n", file);//give error message
+		writelog("data could not be written correctly. Check permission of the file\n");//log error message
+		return;//end function
+	}
+	fclose(data);//close file
 }
 
 
-void load(const char file){//load board to file
+
+
+
+void load(const char* file){//load board to file
+	FILE *data = fopen(file, "rb");//open file for reading
+	if (data == NULL){//if it wasn't opend
+		printf("could not open file. Check if %s exists", file);//give error message
+		writelog("could not open file.Check if file exists");//log error message
+		return;//end function
+	}
+	rewind(data);//set to start
+	int i, j;
+	for (i = 0; i < BOARD_SIZE; i++){//each row
+		for (j = 0; j < BOARD_SIZE; j++){//each position
+			int piece = fgetc(data);//load piece
+			if (piece == EOF){
+				printf("End of file reached. Data is not in a correct format.\n");//give error message
+				writelog("End of file reached. Data is not in a correct format.\n");//log error message
+				return;//end function
+			}
+			board[i][j] = piece;
+			}
+		}
+	int turn = fgetc(data);//load turn
+	if (turn == EOF){
+		printf("End of file reached. Data is not in a correct format.\n");//give error message
+		writelog("End of file reached. Data is not in a correct format.\n");//log error message
+		return;//end function
+	}
+	mode = fgetc(data);//load mode
+	if (mode == EOF){
+		printf("End of file reached. Data is not in a correct format.\n");//give error message
+		writelog("End of file reached. Data is not in a correct format.\n");//log error message
+		return;//end function
+	}
+	fclose(data);//close file
+}
+
+
+
+
+
+
+void writelog(const char* message){//write message in to log file
+	int status = fputs(message, logfile);//write message and get status
+	if (status == EOF){//if data wasn't written
+		printf("Could not write lo log file %s. Plese check the file permission.\n Message was %s\n", LOG, message);//give error message
+		return;//end function
+	}
+	status = fputs("\n", logfile);//write newline
+	if (status == EOF){//if data wasn't written
+		printf("Could not write lo log file %s. Plese check the file permission.\n Message was %s", LOG, message);//give error message
+		return;//end function
+	}
+}
+
+
+
+
+
+
+int legal(unsigned char x, unsigned char y, unsigned char piece){//check if that move is legal
+	return 1;//it is legal
+}
+
+
+
+
+
+void play(void){//make computer play
+	//random play
+	srand(time);//seed rng with time
+	int x = rand() % (BOARD_SIZE);//get random move
+	int y = rand() % (BOARD_SIZE);
+	while (legal(x, y, WHITE) == 0){//while it is not a legal move
+		int x = rand() % (BOARD_SIZE);//get new random move
+		int y = rand() % (BOARD_SIZE);
+	}
+	place(x, y, WHITE);//place that piece
+	char move[64];//move in string
+	sprintf(move, "White move at %d %d\n", x, y);//make move into string
+	printf("%s", move);//print that move
+	writelog(move);//log that move
+	printf("Black's Turn\n");//display turn
+	writelog("Black's Turn\n");//log turn
+}
+
+
+
+
+
+
+
+void pass(unsigned char piece){//pass that turn
+	if (piece == BLACK){//if black is passing
+		black_pass = 1;//set white as passed
+	}
+	else {
+		white_pass = 1;//set black as passed
+	}
 
 }
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>//standard utility
 #include <stdio.h> //input output
 #include <math.h> //math
+#include <time.h> //randomseed
 
 #define BOARD_SIZE 19 //board size
 #define BLACK_PIECE "X " //black piece
@@ -16,18 +17,28 @@
 #define BLANK 0 //blank in array
 #define STAR 3 //star point in aray
 #define LOG "log.txt" //log file for all games
-
+#define HELP "Commands:\n help, -h, h, -help, or blank to display this message \n load to load file. input filename when prompted \n save to save file. input filename when prompted \n place <x> <y> to place piece on that position \n new to get new game \n print to print out bord \n pass to pass \n quit to quit \n"
 
 unsigned char board[BOARD_SIZE][BOARD_SIZE];//go board. blank is 0, black is 1, white is 2. star points is 3. board[y][x].
 unsigned char simulation[BOARD_SIZE][BOARD_SIZE];//copy of go board for simulations and scoring.
+unsigned char turn;//if it is black or white turn
 
 unsigned int black_captured;//number of black captured
 unsigned int white_captured;//number of white captured
+
+unsigned int black_pass;//if black passed
+unsigned int white_pass;//if white passed
 
 unsigned int black_area_score;//area / chinese scoring for black
 unsigned int white_area_score;//area / chinese scoring for white
 unsigned int black_territory_score;//territory / japanese scoring for black
 unsigned int white_territory_score;//territory / japanese scoring for white
+
+unsigned char mode;//game mode. 1 for computer vs human 2 for human vs human black is human. number of human players
+
+
+FILE* logfile;//logfile
+
 
 void printboard(void);//print out bord following the macros with space between each
 
@@ -39,8 +50,16 @@ void territoryscore(void);//count score for territory / japanese scoring
 
 void areascore(void);//count score for area / chinese scoring
 
-void save(const char file);//save board to file
+void save(const char* file);//save board to file
 
-void load(const char file);//load board from file
+void load(const char* file);//load board from file
+
+void writelog(const char* message);//write message in to log file
+
+int legal(unsigned char x, unsigned char y, unsigned char piece);//check if that move is legal
+
+void play(void);//make computer play
+
+void pass(unsigned char piece);//pass that turn
 
 #endif
